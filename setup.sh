4bb51fb2590651633e2523f7f02e80323e859550
Paul Tran <paul.trangiabao@gmail.com>
2025-10-16 11:17:17 -0400

    Added code for program to remove itself after finishing execution.

--- setup.sh
+++ setup.sh
@@ -71,3 +71,5 @@ curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 sudo apt update
 sudo apt install code
+
+rm -rf ./

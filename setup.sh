85f8cdfa182875f3539dd89feee0b479e764b711
Paul Tran <paul.trangiabao@gmail.com>
2025-10-19 15:30:38 +0000

    Added script to install Unity Hub.

--- setup.sh
+++ setup.sh
@@ -80,6 +80,12 @@ sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/micros
 sudo apt update
 sudo apt install code
 
+# Install Unity Hub
+wget -qO - https://hub.unity3d.com/linux/keys/public | gpg --dearmor | sudo tee /usr/share/keyrings/Unity_Technologies_ApS.gpg > /dev/null
+sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/Unity_Technologies_ApS.gpg] https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list'
+sudo apt update
+sudo apt install unityhub
+
 # Install fonts to system
 sudo cp ./fonts/* /usr/share/fonts/
 

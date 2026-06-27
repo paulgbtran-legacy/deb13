04e5b0c29d9bb910c03d4f6bb497fa019a5736d5
Paul Tran <paulgbtran@gmail.com>
2026-06-27 09:37:21 -0400

    Fix wget command for Microsoft Edge installation

--- msedge.sh
+++ msedge.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Install Microsoft Edge
-wget https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_149.0.4022.80-1_amd64.deb?brand=M102
+wget https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_149.0.4022.80-1_amd64.deb
 sudo apt install microsoft-edge-stable_149.0.4022.80-1_amd64.deb

d04a68db42e6551d56f59fcb2156934057d54d31
Paul Tran <paulgbtran@gmail.com>
2026-06-27 09:37:36 -0400

    Update Miniconda installation command
    Removed 'sudo' from the Miniconda installation command.

--- miniconda.sh
+++ miniconda.sh
@@ -2,4 +2,4 @@
 
 # Installing Miniconda
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-sudo bash Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh
